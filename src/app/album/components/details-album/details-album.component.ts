464fec2c572cd39d9463a9faa504ef5c56d9ab97
Juan Florez <juanks339@gmail.com>
2024-12-10 09:20:39 -0500

    NEW: add form to creating comments

--- src/app/album/components/details-album/details-album.component.ts
+++ src/app/album/components/details-album/details-album.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input, SimpleChanges } from '@angular/core';
-import { Album } from '../../interfaces/album.interface';
+import { Component, EventEmitter, Input, Output, SimpleChanges } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-details-album',
@@ -7,15 +7,41 @@ import { Album } from '../../interfaces/album.interface';
   styleUrls: ['./details-album.component.scss']
 })
 export class DetailsAlbumComponent {
-
+  
+  public commentsForm!: FormGroup;
   public isCreateComment: boolean = false;
+  public rating: string[] = ['1', '2', '3', '4', '5'];
 
   @Input() album!: any;
   @Input() isLoading!: boolean | null;
 
+  @Output() emitNewComment: EventEmitter<any> = new EventEmitter();
+
+  constructor(
+    private fb: FormBuilder,
+  ) {}  
+
+  ngOnInit(): void {
+    this.newComment();
+  }
+
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['album']) {
       console.log(this.album);
+    }
+  }
+
+  public newComment(): void {
+    this.commentsForm = this.fb.group({
+      rating: ['', Validators.required],
+      comment: ['', Validators.required],
+    });
+  }
+
+  public saveComment(): void {
+    if (this.commentsForm.valid) {
+      this.emitNewComment.emit(this.commentsForm.value);
+      this.commentsForm.reset();
     }
   }
 }
